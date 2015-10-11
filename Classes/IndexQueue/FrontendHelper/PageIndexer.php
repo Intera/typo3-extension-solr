d2a3d5704a2d29a516b81c0fc4508b1a66b31556
Alexander Stehlik <alexander.stehlik@gmail.com>
2015-10-11 14:09:03 +0200

    [BUGFIX] Use mount point parameter in page document ID
    The mount parameter that is used to access a page is now passed on
    to the page indexer and will be added to the page document ID if
    it is not empty. This allows a page to appear multiple times in the
    index in its different mount point contexts.
    
    Relates: #59438
    Change-Id: I4fd0d4f21b1805fc532eef1a51851ddc8fa303ee

--- Classes/IndexQueue/FrontendHelper/PageIndexer.php
+++ Classes/IndexQueue/FrontendHelper/PageIndexer.php
@@ -261,6 +261,7 @@ class Tx_Solr_IndexQueue_FrontendHelper_PageIndexer extends Tx_Solr_IndexQueue_F
 			$indexer->setSolrConnection($this->getSolrConnection());
 			$indexer->setPageAccessRootline($this->getAccessRootline());
 			$indexer->setPageUrl($this->generatePageUrl());
+			$indexer->setMountPointParameter($GLOBALS['TSFE']->MP);
 
 			$this->responseData['pageIndexed']          = (int)   $indexer->indexPage();
 			$this->responseData['originalPageDocument'] = (array) $indexer->getPageSolrDocument();
